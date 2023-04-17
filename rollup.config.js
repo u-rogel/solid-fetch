6077f57ba6171eba15849baa8e49de86801ff9cf
Uria Rogel <uria_rogel@hotmail.com>
2023-04-17 13:19:57 +0200

    works as ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,48 @@
 import { babel } from '@rollup/plugin-babel'
 import cleanup from 'rollup-plugin-cleanup'
 import { terser } from 'rollup-plugin-terser'
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+
+const minifiedOutputs = [
+  {
+    file: pkg.exports['.'].import,
+    format: 'esm',
+    plugins: [
+      terser({ format: { comments: false } }),
+    ],
+  },
+  {
+    file: pkg.exports['.'].require,
+    format: 'cjs',
+    plugins: [
+      terser({ format: { comments: false } }),
+    ],
+  },
+];
+
+const unMinifiedOutputs = minifiedOutputs.map(({ file, ...rest }) => ({
+  ...rest,
+  file: file.replace('.min.', '.'),
+}));
 
 const config = {
-  input: 'src/index.js',
-  output: {
-    format: 'esm',
-    file: 'dist/bundle.js',
-    plugins: [terser()],
-  },
+  input: './src/index.ts',
+  output: [...unMinifiedOutputs, ...minifiedOutputs],
   plugins: [
+    typescript({
+      tsconfig: false,
+      target: "es2019",
+      rootDir: './src',
+      include: ['**/*'],
+      outDir: './dist',
+      module: "esnext",
+      declaration: true,
+      esModuleInterop: true,
+      forceConsistentCasingInFileNames: true,
+      strict: true,
+      skipLibCheck: true
+    }),
     babel({ babelHelpers: 'bundled' }),
     cleanup(),
   ],
--- src/util/simple-data.js
+++
@@ -1,7 +0,0 @@
-module.exports = (property, funcResolver) => {
-  let value = property
-  if (typeof property === 'function') {
-    value = property(funcResolver())
-  }
-  return value
-}
